668202eaddeb47c3746b2c073daaeae3c31042de
DESKTOP-SE0GAC1\JeffreyLau <jeffylau007@gmail.com>
2019-03-10 22:02:02 +1000

    important backend setting

---
+++ server/config/config.js
@@ -0,0 +1,27 @@
+require("dotenv").config(); // this is important!
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "dev",
+    host: process.env.DB_HOST,
+    port: 5432,
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "testing",
+    host: process.env.DB_HOST,
+    port: 5432,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "production",
+    host: process.env.DB_HOST,
+    port: 5432,
+    dialect: "postgres"
+  }
+};
