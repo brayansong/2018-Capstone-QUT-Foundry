70064bd16ee6e031b22d4ea53f483e99f2987c49
DESKTOP-SE0GAC1\JeffreyLau <jeffylau007@gmail.com>
2019-05-22 00:45:34 +1000

    client side

--- client/src/constants/server.js
+++ client/src/constants/server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 const SERVER_URL = process.env.NODE_ENV === "production" ?
-    "https://www.jeffreylaudev.tk:3000" :
-    "https://localhost:3000"
+    "https://www.jeffreylaudev.tk:8443" :
+    "https://localhost:8443"
 
 export default SERVER_URL;
 
